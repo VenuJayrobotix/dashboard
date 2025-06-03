264915a47bd9280ee316176430c9adf4ebd939cc
VenuJayrobotix <venu.jayrobotix@gmail.com>
2025-06-03 23:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def on_connect():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    #socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+    socketio.run(app)
